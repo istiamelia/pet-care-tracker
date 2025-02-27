adbe8b42ab87224d5db0d86edc54b3a9a5cb05e1
istiamelia <istiamelia7@gmail.com>
2025-02-27 21:38:32 +0700

    Added alert message for email and password input for signup form

---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+const submitBtn = document.querySelector(".submitBtn");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const usernameInput = document.querySelector("#username");
+const alertEmailDiv = document.querySelector(".alertEmailDiv");
+const alertEmail = document.createElement("p");
+const alertPwdDiv = document.querySelector(".alertPwdDiv");
+const alertPwd = document.createElement("p");
+const alertUsernameDiv = document.querySelector(".alertUsernameDiv");
+const alertUsername = document.createElement("p");
+
+submitBtn.addEventListener("click", (event) => {
+  let emailInputVal = document.querySelector("#email").value;
+  let passwordInputVal = document.querySelector("#password").value;
+  let usernameInputVal = document.querySelector("#username").value;
+  if (!emailInputVal) {
+    alertEmail.innerHTML = "Your email is still empty";
+    alertEmailDiv.append(alertEmail);
+    event.preventDefault();
+  }
+  if (!passwordInputVal) {
+    alertPwd.innerHTML = "Your password is still empty";
+    alertPwdDiv.append(alertPwd);
+    event.preventDefault();
+  }
+  if (!usernameInputVal) {
+    alertUsername.innerHTML = "Your username is still empty";
+    alertUsernameDiv.append(alertUsername);
+    event.preventDefault();
+  }
+});
+
+emailInput.addEventListener("input", (event) => {
+  let InputVal = event.target.value;
+  if (!InputVal) {
+    alertEmail.innerHTML = "Your email is still empty";
+    alertEmailDiv.append(alertEmail);
+  } else {
+    alertEmail.innerHTML = ""; // Clears the message if input is filled
+  }
+});
+passwordInput.addEventListener("input", (event) => {
+  let InputVal = event.target.value;
+  if (!InputVal) {
+    alertPwd.innerHTML = "Your password is still empty";
+    alertPwdDiv.append(alertPwd);
+  } else {
+    alertPwd.innerHTML = ""; // Clears the message if input is filled
+  }
+});
+usernameInput.addEventListener("input", (event) => {
+  let InputVal = event.target.value;
+  if (!InputVal) {
+    alertUsername.innerHTML = "Your username is still empty";
+    alertUsernameDiv.append(alertUsername);
+  } else {
+    alertUsername.innerHTML = ""; // Clears the message if input is filled
+  }
+});
